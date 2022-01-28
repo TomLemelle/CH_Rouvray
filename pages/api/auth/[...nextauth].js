6c3c83a727532f6c526bc6df8dc92de58d6178da
Tom Lemelle <tom.lemelle@hotmail.fr>
2022-01-28 23:49:30 +0100

    WIP auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,77 +1,26 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google"
-import Credentials from "next-auth/providers/credentials";
-import axios from 'axios'
 
-
-const providers = [
-    GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_SECRET_ID,
-        authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
-    }),
-    Credentials({
-        name: 'Credentials',
-        authorize: async (credentials) => {
-            try {
-
-                const user = await axios.post('http//localhost:3000/register',
-                    {
-                    user: {
-                        email: credentials.email,
-                        firstname: credentials.firstname,
-                        lastname: credentials.lastname,
-                        password: credentials.password,
-                    }
-                    },
-                    {
-                    headers: {
-                        accept: '*/*',
-                        'Content-Type': 'application/json'
-                    }
-                    })
-
-                if (user) {
-                    return user
-                } else {
-                    return null
-                }
-
-            } catch (e) {
-                const errorMessage = e.response.data.message
-                // Redirecting to the login page with error message          in the URL
-                throw new Error(errorMessage + '&email=' + credentials.email)
-            }
-        },
+export default (req, res) => 
+    NextAuth(req, res, {
+        providers: [
+            GoogleProvider({
+                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientSecret: process.env.GOOGLE_SECRET_ID,
+                authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth?prompt=consent&access_type=offline&response_type=code'
+            }),
+        ],
         debug: process.env.NODE_ENV === 'development',
         secret: process.env.AUTH_SECRET,
         jwt: {
             secret: process.env.JWT_SECRET,
         },
-    })
-]
-
-const callbacks = {
-  // Getting the JWT token from API response
-  async jwt(token, user) {
-    if (user) {
-        token.accesToken = user.data.token
-    }
-    return token
-  },
-
-  async session(session, token) {
-    session.accessToken = token.accessToken
-    return session
-  }
-}
-
-const options = {
-  providers,
-  callbacks,
-  pages: {
-    error: '/'
-  }
-}
-
-export default (req, res) => NextAuth(req, res, options)
+        /*callbacks: {
+            async signIn({ account, profile }) {
+                if(account.provider === "google") {
+                    return profile.email_verified && profile.email.endsWith("@normandiewebschool.fr")
+                }
+                return console.log('account not verified')
+            },
+        }*/
+});
